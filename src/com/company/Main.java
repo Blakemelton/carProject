956fa24097b1903783fec45f56b2aceb8f2a89ad
blakemelton <blakemelton2591@gmail.com>
2021-11-03 20:14:18 -0400

    the blue ferarri car better than the Bob car in stash if you want to see Bob let me know

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         Car newcar = new Car();
-        
+        newcar.name = "Feerarrrii";
+        newcar.color = "blue";
+        newcar.doors=3;
+        newcar.engineCapacity="70000cc";
+
+        System.out.println(newcar.color);
     }
 }
